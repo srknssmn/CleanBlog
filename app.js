b1f0c0f57c17995dd04ee9d17260b398488e3038
Serkan Sisman <srknssmn@gmail.com>
2022-12-18 22:39:11 +0300

    Proje yapisi statik ve dinamik olarak degistirildi.

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 
+// Dinamik dosyalar views klasöründe aktif edildi.
+app.set("view engine" , "ejs");
+
+// Statik dosyalar public klasöründe aktif edildi.
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    const message = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(message);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
